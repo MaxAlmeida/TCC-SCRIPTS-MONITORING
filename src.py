e44e3bd71d45ab0ca87db92324195e002860f8e6
MaxAlmeida <llewxam100@hotmail.com>
2016-06-17 15:33:31 +0000

    refactoring script

--- src.py
+++ src.py
@@ -2,17 +2,20 @@ import subprocess
 import re
 import time
 
-cont = 0
-avg_sum = 0 
-while cont < 300:
-  process = subprocess.Popen("munin-run diskstats |grep vda_wrio.value",shell=True, stdout=subprocess.PIPE,)
-  output = process.communicate()[0].rstrip('\n')
-  final =  re.findall(r'[-+]?([0-9]*\.[0-9]+|[0-9]+)',output)
-  avg_sum +=  float(final[0])
-  print final[0]
-  time.sleep(1) 		
-  cont+=1
+
+def colect_metric(amount):
+  cont = 0
+  avg_sum = 0 
+  while cont < amount:
+    colected_metric = subprocess.Popen("munin-run diskstats |grep vda_wrio.value",shell=True, stdout=subprocess.PIPE,)
+    colected_metric = colected_metric.communicate()[0].rstrip('\n')
+    metric_value =  re.findall(r'[-+]?([0-9]*\.[0-9]+|[0-9]+)',colected_metric)
+    avg_sum +=  float(metric_value[0])
+    print metric_value[0]
+    time.sleep(1) 		
+    cont+=1
    
-avg =  avg_sum/300
-print "media: ", avg
-   
+  avg =  avg_sum/amount
+  print "media: ", avg
+  
+colect_metric(5) 
