7fe246f1f9a71cea6b223c6686ff7f923a24de0e
MaxAlmeida <llewxam100@hotmail.com>
2016-06-19 15:49:41 +0000

    Adding method to calculate average metrics

--- src.py
+++ src.py
@@ -3,7 +3,9 @@ import re
 import time
 WRIO = "munin-run diskstats |grep vda_wrio.value"
 RDIO = "munin-run diskstats |grep vda_rdio.value"
-def collect_metric(amount,metric_munin):
+
+#Colects metric from munin
+def collect_metric(metric_munin):
   cont = 0
   avg_sum = 0 
   colected_metric = subprocess.Popen(metric_munin,shell=True, stdout=subprocess.PIPE,)
@@ -12,20 +14,23 @@ def collect_metric(amount,metric_munin):
   print metric_munin,":",metric_value[0]
   return float( metric_value[0])	  
  
- 
-cont = 0
-sum_wrio  = 0
-sum_rdio = 0
-while(cont < 10):
-    value_wrio = collect_metric(5,WRIO) 
-    value_rdio = collect_metric(5,RDIO)
-    sum_wrio += value_wrio
-    sum_rdio += value_rdio	 
-    print "\n"
-    time.sleep(0.5)
-    cont+=1
-avg_wrio = sum_wrio/10
-avg_rdio = sum_rdio/10 
+#calculates the average metric given time
+def calculate_average(time_average):
+  cont = 0
+  sum_wrio  = 0
+  sum_rdio = 0
+  while(cont < time_average):
+     value_wrio = collect_metric(WRIO) 
+     value_rdio = collect_metric(RDIO)
+     sum_wrio += value_wrio
+     sum_rdio += value_rdio	 
+     print "\n"
+     time.sleep(1) #Collects every 1 second
+     cont+=1
+  avg_wrio = sum_wrio/time_average
+  avg_rdio = sum_rdio/time_average 
 
-print "media WRIO: ",avg_wrio
-print "media RDIO: ",avg_rdio   	
+  print "media WRIO: ",avg_wrio
+  print "media RDIO: ",avg_rdio 
+
+calculate_average(10)  	
