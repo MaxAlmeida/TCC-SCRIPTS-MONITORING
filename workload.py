ed24a33469ac72d7c25a57f90b049b686a2f31d4
MaxAlmeida <llewxam100@hotmail.com>
2016-07-19 00:05:40 +0000

    adding bzip2 method benchmark

--- workload.py
+++ workload.py
@@ -1,6 +1,7 @@
 import subprocess
 import re
 import os
+import time
 
 def run_add_double(times):
   report_file = 'suite9.ss'
@@ -24,4 +25,25 @@ def run_add_double(times):
       sum_average += int(value[0])
   print float(sum_average)/float(times) 
  
-run_add_double(3)
+def run_bzip2(times):
+  report_file = 'report_file'
+  command = "(time sh -c 'bzip2 -c --best file.txt > file.txt.bz') 2>> report_file"
+  os.chdir('/root/huge-file')
+ 
+  if os.path.isfile(report_file):
+    os.remove(report_file)
+ 
+  cont = 1
+  while cont <= times:
+    os.system(command)
+    cont+=1
+ 
+  #calculate average
+  sum_average = 0
+  score_file = open(report_file, 'r')
+  for line in score_file:
+    if "real" in line:
+      value = re.findall(r'[-+]?([0-9]*\.[0-9]+|[0-9]+)',line)
+      sum_average += float(value[1])
+  print float(sum_average/float(times))
+
