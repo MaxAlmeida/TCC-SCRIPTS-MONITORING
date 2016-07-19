7e6dd8a1fba3b7e0e01c6cc12e81be6ef1893ffb
MaxAlmeida <llewxam100@hotmail.com>
2016-07-19 01:15:10 +0000

    Refactoring time elapsed score

--- workload.py
+++ workload.py
@@ -3,6 +3,28 @@ import re
 import os
 import time
 
+def average_time_elapsed(command, times, directory):
+  report_file = 'report_file'
+  os.chdir(directory)
+ 
+  if os.path.isfile(report_file):
+    os.remove(report_file)
+ 
+  cont = 1
+  while cont <= times:
+    os.system(command)
+    cont+=1
+ 
+  #calculate average
+  sum_average = 0
+  score_file = open(report_file, 'r')
+  for line in score_file:
+    if "real" in line:
+      value = re.findall(r'[-+]?([0-9]*\.[0-9]+|[0-9]+)',line)
+      sum_average += float(value[1])
+  return float(sum_average/float(times))
+
+
 def run_add_double(times):
   report_file = 'suite9.ss'
   os.chdir('/root/aimbench/caldera/suite9')
@@ -26,24 +48,13 @@ def run_add_double(times):
   print float(sum_average)/float(times) 
  
 def run_bzip2(times):
-  report_file = 'report_file'
   command = "(time sh -c 'bzip2 -c --best file.txt > file.txt.bz') 2>> report_file"
-  os.chdir('/root/huge-file')
+  directory = '/root/huge-file'
+  time_score = average_time_elapsed(command,times,directory)
+  print time_score 
  
-  if os.path.isfile(report_file):
-    os.remove(report_file)
- 
-  cont = 1
-  while cont <= times:
-    os.system(command)
-    cont+=1
- 
-  #calculate average
-  sum_average = 0
-  score_file = open(report_file, 'r')
-  for line in score_file:
-    if "real" in line:
-      value = re.findall(r'[-+]?([0-9]*\.[0-9]+|[0-9]+)',line)
-      sum_average += float(value[1])
-  print float(sum_average/float(times))
+run_bzip2(4) 
+
+#def run_grep(times):
+  
 
